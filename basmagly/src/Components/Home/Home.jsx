b397e34850759c0d010d3d4e7aa5451d57333ad5
Mohamed1Hatem <mohamedhatemm357@gmail.com>
2024-12-09 01:30:22 +0200

    <Add home page>

---
+++ basmagly/src/Components/Home/Home.jsx
@@ -0,0 +1,76 @@
+import React, { Children, useEffect, useState} from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import { FaBars, FaHome } from "react-icons/fa";
+import { BiFile, BiChat, BiTask } from 'react-icons/bi';
+import './Home.css'
+
+const Home = ({Children}) => {
+
+    
+    useEffect(() => {
+        document.title = "Home - Basmagly"; // Set the title
+    }, []); // Runs once when the component mounts
+
+    const[isOpen ,setIsOpen] = useState(false);
+    const toggle = () => setIsOpen (!isOpen);
+
+    const menuItem = [
+        { path: "default", name: "Home", icon: <FaHome /> },
+        { path: "documents", name: "Documents", icon: <BiFile /> },
+        { path: "chatbot", name: "Chatbot", icon: <BiChat /> },
+        { path: "quizzes", name: "Quizzes", icon: <BiTask /> },
+    ];
+    
+
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
+  return (
+    <div className="container">
+        <div style={{width: isOpen ? "250px" : "50px"}} className="sidebar">
+            <div className="top_section">
+                <h1 style={{display: isOpen ? "block" : "none"}} className="logo">Basmagly</h1>
+                <div style={{marginLeft: isOpen ? "40px" : "0px"}} className="bars">
+                    <FaBars onClick={toggle}/>
+                </div>
+            </div>
+            {
+                menuItem.map((item, index)=>(
+                    
+                    <NavLink to={item.path} key={index} className="link" activeclassName="active_section">
+                        <div className="icon">{item.icon}</div>
+                        <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
+                    </NavLink>
+                ))
+            }
+        </div>
+        <main className="main-content"> {Children}
+            <div className="navbar">
+                
+                <div className="links">
+                    <a href="#about">About Us</a>
+                    <a href="#contact">Contact Us</a>
+                </div>
+                <div className="user-menu" onClick={toggleDropdown}>
+                    <div className="user-photo">
+                        <img src="/images/default-avatar-profile-icon-of-social-media-user-vector.jpg" alt="User" />
+                    </div>
+                    <div className={`dropdown ${isDropdownOpen ? "show" : ""}`}>
+                        <a href="/profile">Profile</a>
+                        <a href="/logout">Log Out</a>
+                    </div>
+                </div>
+            </div>
+            <Outlet />
+        </main>
+
+    </div>
+        
+  );
+};
+
+export default Home;
+
